f41f8672632997d8b73ba220926773d48ddb9c8c
ThallesDenner <ctdf.engmec@gmail.com>
2022-03-07 20:16:54 -0300

    Resolução da mesa de trabalho de Front-End II - Aula 04

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+const itensLista = document.getElementsByClassName('lista__item');
+
+itensLista[0].classList.add('removido');
+itensLista[0].innerText = 'Carro - Removido';
+
+itensLista[1].classList.add('selecionado');
+itensLista[1].innerText = 'Bike - Selecionado';
+
+itensLista[2].classList.add('selecionado');
+itensLista[2].innerText = 'Patinete - Selecionado';
+
+itensLista[3].classList.add('removido');
+itensLista[3].innerText = 'Celular - Removido';
+
+itensLista[4].classList.add('removido');
+itensLista[4].innerText = 'Carregador - Removido';
